5e2239dd10b9415ce8dc111329a7b4891ee4b38e
Ricardo Malta <ricardo@cloudoki.com>
2016-08-12 16:43:16 +0100

    basic routes
    POST /api/find to as the slack docs says and receive the proper myinstant

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const express = require('express'),
+      router = express.Router(),
+      myinstants = require('../lib/myinstants');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  myinstants.list((err, body) => {
+
+    res.json(body);
+  });
+
+});
+
+router.get('/find', function(req, res, next) {
+
+  var text = req.query && req.query.name ? req.query.name : "";
+
+  myinstants.find(text, (err, body) => {
+
+    res.json(body);
+  });
+
+});
+
+router.post('/find', function(req, res, next) {
+
+  var text = req.body && req.query.text ? req.query.text : "";
+
+  myinstants.find(text, (err, body) => {
+
+    res.json(body);
+  });
+
+});
+
+// router.get('/random', function(req, res, next) {
+//
+//   myinstants.random((err, response) => {
+//     // res.set("Content-Length", response.headers['content-length']);
+//     // res.set("Content-Type",response.headers['content-type']);
+//     // res.set(response.headers)
+//     res.json(response);
+//     // res.send(response.headers);
+//     // res.write(res.body, 'binary');
+//     // res.end();
+//   });
+//
+// });
+
+module.exports = router;
